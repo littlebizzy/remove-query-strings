20812b3243a0b852c69506964702bf7d1bdd7ddb
Jesse Nickles <jesse@littlebizzy.com>
2017-10-17 13:44:08 -0700

    Update remove-query-strings.php

--- remove-query-strings.php
+++ remove-query-strings.php
@@ -3,7 +3,7 @@
 Plugin Name: Remove Query Strings
 Plugin URI: https://www.littlebizzy.com/plugins/remove-query-strings
 Description: Removes all query strings from static resources meaning that proxy servers and beyond can better cache your site content (plus, better SEO scores).
-Version: 1.2.4
+Version: 1.2.5
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
@@ -14,21 +14,19 @@ Prefix: RMQRST
 
 /* Initialization */
 
-// Avoid script calls via plugin URL
+// Block direct calls
 if (!function_exists('add_action'))
 	die;
 
-// This plugin constants
+// Plugin constants
 define('RMQRST_FILE', __FILE__);
 define('RMQRST_PATH', dirname(RMQRST_FILE));
-define('RMQRST_VERSION', '1.2.4');
+define('RMQRST_VERSION', '1.2.5');
 
-// Quick context check
-if ( is_admin() ) {
-	require_once( RMQRST_PATH.'/admin-notices.php' );
-	RMQRST_Admin_Suggestions::instance();
-	return;
-}
+// Admin Notices module
+require_once(dirname(__FILE__).'/admin-notices.php');
+RMQRST_Admin_Notices::instance(__FILE__);
+register_uninstall_hook(__FILE__, array('RMQRST_Admin_Notices', 'uninstall'));
 
 
 /* WP hooks */
